0ffa3b23fbba9b6a6427f3b9a45bf272de7c0370
Alina <alina.bitochkina96@mail.ru>
2023-09-20 00:09:30 +0500

    added array library

---
+++ c#/lesson_2/array_library/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace array_library
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+            /*создаём функцию, которая будет заполнять массив
+             * случайными значениями из заданного диапазона
+             */
+
+         
+        {
+            void FillArray(int[] collection)
+            {
+                int length = collection.Length;
+                int index = 0;
+                Random rnd = new Random();
+                while (index < length) 
+                {
+                    collection[index] = rnd.Next(1, 10);
+                    index++;
+                    
+                }
+            }
+            //функция 
+            void PrintArray(int[] col)
+            {
+                int count = col.Length;
+                int position = 0;   
+                while (position < count) 
+                {
+                    Console.WriteLine(col[position]);   
+                    position++; 
+                }
+            }
+            // функция на поиск заданного числа
+            int IndexOf(int[] collection, int find)
+            {
+                int count = collection.Length;
+                int index = 0;
+                int position = -1; //на случай если искомое число не будет в массиве элементов, если бы стоял 0, казалось бы, что индекс искомого 0
+                while (index < count)
+                {
+                    if (collection[index] == find)
+                    {
+                        position = index;
+                        break;
+                    }
+                    index++;
+                }
+
+                return position;
+            }
+                //это был первый шаг, определили массив из 10 элементов    
+
+            int[] array = new int[10];
+
+            //вызвали метод FillArray который заполнил массив
+            // и метод PrintArray который вывел массив
+            FillArray(array);
+            PrintArray(array);
+            Console.WriteLine();
+
+                int pos = IndexOf(array, 4);
+                Console.WriteLine(pos);
+
+            }
+    }
+}
---
+++ c#/lesson_2/cycle_finding_index_n/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cycle_finding_index_n
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 14, 21, 334, 44, 513, 6, 21, 75, 86 }; 
+            int n = array.Length;
+            int find = 21;
+            int index = 0;
+
+            while (index < n)
+            {
+                if (array[index] == find)
+                {
+                    Console.WriteLine(index);
+                    break;
+                    /*если в массиве несколько одинаковых элементов, 
+                     * можно принудительно остановить поиск словом
+                     * break
+                     */
+                }
+                //index += 1;
+                index++;
+
+            }
+        }
+    }
+}
