444880ebfe08ef256de26c60e83c285616d85151
Alina <alina.bitochkina96@mail.ru>
2023-09-19 22:41:17 +0500

    added lessons functions and array

---
+++ c#/lesson_2/function/function/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Max (int arg1, int arg2, int arg3)
+                {
+                int result = arg1;
+                if (arg2 > result) result = arg2;
+                if (arg3 > result) result = arg3;
+                return result;
+                } 
+            int a1 = 3;
+            int b1 = 4;
+            int c1 = 5;
+            int a2 = 8;
+            int b2 = 900;
+            int c2 = 10;
+            int a3 = 1500;
+            int b3 = 16;
+            int c3 = 17;
+
+
+            //int max1 = Max(a1, b1, c1);
+            //int max2 = Max(a2, b2, c2);
+            //int max3 = Max(a3, b3, c3);
+            //int max = Max(max1,max2, max3);
+            //Console.WriteLine(max); 
+
+            /*Но можно сделать функцию в функции
+             * Это называется функциональное программирование
+             * Мы можем внутри аргумента передать функцию
+             */
+            int max = Max(
+                Max(a1, b1, c1),
+                Max(a2, b2, c2),
+                Max(a3, b3, c3));
+            Console.WriteLine(max);
+
+
+
+
+
+        }
+    }
+}
---
+++ c#/lesson_2/intro_arrays/intro_arrays/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace intro_arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   // задаем функцию Мах
+            int Max(int arg1, int arg2, int arg3)
+            {
+                int max = arg1;
+                if (arg2 > max) max = arg2;
+                if (arg3 > max) max = arg3;
+                return max;
+            }//индекс       0  1   2    3   4     5   6   7   8
+            int[] array = { 5, 8, 15, 256, 35, 1236, 51, 25, 56 };
+            // используем в аргументах функции Мах снова функции Мах
+            int result = Max(
+                Max(array[0], array[1], array[2]),
+                Max(array[3], array[4], array[5]),
+                Max(array[6], array[7], array[8])
+                );
+            Console.WriteLine(result);
+        }
+        //Static int Max(int arg1, int arg2, int arg3)
+        //{
+        //    int result = arg1;
+        //    if (arg2 > result) result= arg2;    
+        //    if (arg3 > result) result= arg3;
+        //    return result;  
+        //}
+    }
+}
